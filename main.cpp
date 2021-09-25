b6b33cb3778fa8251f211e1a4a894c91390f51d9
CheremisinovAlexey <91156840+CheremisinovAlexey@users.noreply.github.com>
2021-09-25 10:52:52 +0300

    e

--- main.cpp
+++ main.cpp
@@ -1,6 +1,72 @@
 #include <iostream>
+using namespace std;
+
+class Point
+{
+public:
+    int y;
+    int x;
+    Point(int x, int y);
+    Point();
+
+    int get_x() { return x; }
+    int get_y() { return y; }
+
+    void set_x(int x1) { x = x1; }
+    void set_y(int y1) { y = y1; }
+
+    Point operator+(Point);
+    Point operator-(Point);
+    int operator (Point);
+    Point operator*(int);
+};
+
+Point::Point(int x1, int y1)
+{
+    x = x1;
+    y = y1;
+}
+
+Point::Point()
+{
+    x = 0;
+    y = 0;
+}
+
+Point Point::operator+(Point a)
+{
+    return Point(x + a.x, y + a.y);
+}
+
+Point Point::operator-(Point a)
+{
+    return Point(x - a.x, y - a.y);
+}
+
+Point Point::operator*(int a)
+{
+    return Point(x * a, y * a);
+}
+
+//Point Point::operator*(Point a)
+//{
+    //return Point((x * a.y) - (y * a.x), 0);
+//}
+
+ostream& operator<<(ostream& out, const Point& a)
+{
+    return out << a.x << "; " << a.y;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int x, y, h;
+    cin>>x>>y>>h;
+    Point a(x, y);
+    Point b(x, y);
+    cout<<a + b<<endl;
+    cout<<a * h<<endl;
+    //cout<<a * b<<endl;
     return 0;
 }
